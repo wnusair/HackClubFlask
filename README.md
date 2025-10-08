b3ab21387423c9fbf3d7a8e787d681e8e7482eb7
Wissam Nusair <75694885+wnusair@users.noreply.github.com>
2025-10-08 21:34:01 +0000

    storing data in CSV. Regsiter and Log in

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HackClubFlask
+
+## 10-1-2025
+We created forms!
+
+## 10-9-2025
+We are storing data in CSV files! Register and login.
--- main.py
+++ main.py
@@ -1,33 +1,47 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 
+import csv
+
 app = Flask(__name__)
 app.secret_key = ";lkfaofiewqjfojdsafjsa"
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
+        full_name = request.form.get("full_name")
+        email = request.form.get("email")
 
-        if username == "shamgar" and password == "shamgar":
-            flash("Success")
-            return redirect(url_for("success"))
-        else:
-            flash("Failed")
-            return redirect(url_for("fail"))
+        with open("accounts.csv", "a") as file:
+            file.write(f"{username},{password},{full_name},{email}\n")
+        
 
+    return render_template("register.html")
 
-    shamgar = "cool"
+@app.route('/login', methods=["GET", "POST"])
+def login():
 
-    return render_template("index.html", x=2, shamgar=shamgar)
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-@app.route('/success')
-def success():
-    return render_template("success.html")
+        with open("accounts.csv", "r", newline="") as file:
+            csv_reader = csv.reader(file)
 
-@app.route('/fail')
-def fail():
-    return render_template("fail.html")
+            for row in csv_reader:
+                if row[3] == email and row[1] == password:
+                    print("account matched", email, password)
+                else:
+                    print("did not match")
+                    print(row[3], email, row[1], password)
 
+    return render_template("login.html")
 
 app.run('0.0.0.0')
